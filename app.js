87e2cf4939d167de1ae9c66dc7568b344438bd2d
Eduardo DuQuesne <eduquesne@gmail.com>
2018-03-16 09:40:53 -0500

    Completed Egg Exercise

--- app.js
+++ app.js
@@ -25,13 +25,26 @@ app.use('/see-our-chickens', (req, res, next) => {
     });
 });
 app.use('/see-our-eggs', (req, res, next) => {
-    // console.log('        ,ggadddd8888888bbbbaaa,_ ' );
-    // console.log('     ,ad888,      `Y88,      `Y888baa,' );
-    // console.log('   ,dP"  "Y8b,      `"Y8b,      `"Y8888ba,' );
-    // console.log('  ,88      "Y88b,      `"Y8ba,       `"Y88Ya,' );
-    // console.log(' ,P88b,      `"Y88b,       `"Y8ba,_       ""8a,' );
-    // console.log(""`,P'"Y88b,        "Y88b,        `"Y8ba,_      `Ya,`" );
-    // res.sendFile('egg.txt', {root: __dirname});
+    const date = new Date();
+console.log(`
+You found the Easter Egg at ${date}
+     ,ggadddd8888888bbbbaaa,_ 
+    ,ad888,      'Y88,      'Y888baa,
+  ,dP"  "Y8b,      '"Y8b,      '"Y8888ba,
+ ,88      "Y88b,      '"Y8ba,       '"Y88Ya,
+,P88b,      '"Y88b,       '"Y8ba,_       ""8a,
+,P'"Y88b,        "Y88b,        '"Y8ba,_      'Ya,
+8'    "Y88b,        ""Y8ba,         ""Y8ba,_   '8,
+8b       "Y88b,         ""Y8ba,_         ""Y88baaY
+88b,        "Y88ba,         ""Y88ba,_         '""P
+8Y88ba,        ""Y88ba,_         ""Y88ba,,_    ,P'
+'b,"Y88ba,         ""Y88baa,_         """Y888bd"
+'b, '"Y88ba,_         ""Y888baa,_         ,8"
+ '8,   '""Y88ba,_         '"""Y8888baaaaaP"
+  'Ya,     '""Y888ba,_           '"d88P"
+    '"Yb,,_     '""Y888baa,__,,adP""'
+        '"""YYYY8888888PPPP"""'
+        `);
     console.log();
     res.sendFile('public/see-our-eggs.html', {
         root: __dirname
